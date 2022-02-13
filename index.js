9f6ccfb5d3b8240214d28a666dbdabdb26ffe2be
Remy Lartiguelongue <remy.lartiguelongue@gmail.com>
2022-02-13 20:33:01 +0100

    Replacing dotenv package by config.json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+const config = require('./config.json');
 const puppeteer = require('puppeteer');
 const mongoose = require('mongoose');
 const Balance = require('./models/Balance');
 const currencyToNumber = require('./utils/CurrencyToNumber');
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_HOST}/${config.MONGO_DB}?retryWrites=true&w=majority`);
 
 (async () => {
 	const browser = await puppeteer.launch();
@@ -14,8 +14,8 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PA
 	// Filling and submitting fields in login form
 	await page.waitForSelector('input[name=username]');
 
-	await page.$eval('input[name=username]', (el, user) => el.value = user, process.env.LINXO_USER);
-	await page.$eval('input[name=password]', (el, password) => el.value = password, process.env.LINXO_PASSWORD);
+	await page.$eval('input[name=username]', (el, user) => el.value = user, config.LINXO_USER);
+	await page.$eval('input[name=password]', (el, password) => el.value = password, config.LINXO_PASSWORD);
 
 	await page.click('button[type=submit]');
 
